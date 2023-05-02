638f83c97e63b52260b5bceec3f6ad1f95e074be
Tanay Bhuta <tanaybhuta07@gmail.com>
2023-05-02 06:16:10 +0530

    Added my project

--- src/COMPONENTS/Banners/BannerSlider.js
+++ src/COMPONENTS/Banners/BannerSlider.js
@@ -6,21 +6,28 @@ const BannerSlider = () => {
     const data = [
         {
             id: 1,
-            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-            title: 'Fresh Vegetables & Fruits at your doorstep',
-            description: 'We deliver fresh vegetables & fruits at your doorstep',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXvoCkB8K0Pi7IjPbsTSIHwdykMTVKqXhtrA&usqp=CAU',
+            title: 'Pet food & treats at your doorstep',
+            description: 'One stop solution for your pet',
             button: 'htttps://www.google.com'
         },
         {
             id: 2,
-            image: 'https://images.unsplash.com/photo-1495480137269-ff29bd0a695c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80',
-            title: 'Fresh Vegetables & Fruits at your doorstep',
-            description: 'Cherries and berries. Sweet peaches and nectarines. Summer baking season is here, and I couldn’t be more delighted.',
+            image: 'https://img.freepik.com/premium-photo/pet-care-concept-various-pet-accessories-tools-yellow-background-flat-lay_154515-6903.jpg',
+            title: 'Toys for your pet',
+            description: 'One stop solution for your pet',
+            button: 'htttps://www.google.com'
+        },
+        {
+            id: 3,
+            image: 'https://cdn.hswstatic.com/gif/dog-leash.jpg',
+            title: ' Belts , Leashes & other accessories at your doorstep',
+            description: 'One stop solution for your pet',
             button: 'htttps://www.google.com'
         }
     ]
 
-
+   
     var settings = {
         dots: true,
         infinite: true,
